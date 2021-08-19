f769ba7c098b53d86e258eb22fe9245d5c917be0
Muskan Dubey <muskandubey28112000@gmailcom>
2021-08-19 23:09:57 +0530

    purchase, input btn updated

--- app.js
+++ app.js
@@ -14,3 +14,24 @@ function slideImage(){
     document.querySelector('.img-showcase').style.transform = `translateX(${-(imgId-1)*displayWidth}px)`;
 }
  window.addEventListener('resize', slideImage);
+ document.querySelector(".minus-btn").setAttribute("disabled","disabled");
+
+ var valueCount
+
+ document.querySelector(".plus-btn").addEventListener("click", function(){
+    valueCount = document.getElementById("quantity").value;
+    valueCount++;
+    document.getElementById("quantity").value = valueCount
+    if(valueCount>1){
+        document.querySelector(".minus-btn").removeAttribute("disabled")
+    }
+ })
+ 
+ document.querySelector(".minus-btn").addEventListener("click", function(){
+    valueCount = document.getElementById("quantity").value;
+    valueCount--;
+    document.getElementById("quantity").value = valueCount
+    if(valueCount==1){
+        document.querySelector(".minus-btn").setAttribute("disabled", "disabled")
+    }
+ })
